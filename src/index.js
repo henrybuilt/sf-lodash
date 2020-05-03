a7273714a5a880762caede89cb1f037bec28a31a
maxhudson <max@henrybuilt.com>
2020-05-03 10:16:23 -0700

    warning

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const filter = require('./filter/filter');
 const findMap = require('./find-map/find-map');
 const objectFromKeys = require('./object-from-keys/object-from-keys');
 
+//WARNING for some reason mocha + babel doesn't like spread syntax
 var _ = Object.assign({}, lodash, {filter, findMap, objectFromKeys});
 
 module.exports = _;
