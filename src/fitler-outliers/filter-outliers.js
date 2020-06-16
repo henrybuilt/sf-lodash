4aa58b31db3f3cf3dc2e2ddd53c776867d63d791
Max Hudson <maxhudson@users.noreply.github.com>
2020-06-16 13:28:02 -0700

    Update filter-outliers.js

--- src/fitler-outliers/filter-outliers.js
+++ src/fitler-outliers/filter-outliers.js
@@ -1,32 +1,25 @@
 import _ from 'lodash';
 
 export default function filterOutliers(array) {
-  // Copy the values, rather than operating on references to existing values
-    var values = array.concat();
+  var values = array.concat();
 
-    // Then sort
-    values.sort( function(a, b) {
-      return a - b;
-    });
+  values.sort(function(a, b) {
+    return a - b;
+  });
 
-    /* Then find a generous IQR. This is generous because if (values.length / 4)
-     * is not an int, then really you should average the two elements on either
-     * side to find q1.
-     */
-    var q1 = values[Math.floor((values.length / 4))];
-    // Likewise for q3.
-    var q3 = values[Math.ceil((values.length * (3 / 4)))];
-    var iqr = q3 - q1;
+  var q1 = values[Math.floor((values.length / 4))];
+  var q3 = values[Math.ceil((values.length * (3 / 4)))];
+  
+  var iqr = q3 - q1;
 
-    // Then find min and max values
-    var maxValue = q3 + iqr*1.5;
-    var minValue = q1 - iqr*1.5;
+  // Find min and max values
+  var maxValue = q3 + iqr*1.5;
+  var minValue = q1 - iqr*1.5;
 
-    // Then filter anything beyond or beneath these values.
-    var filteredValues = values.filter(function(x) {
-        return (x <= maxValue) && (x >= minValue);
-    });
+  // Filter anything beyond or beneath these values.
+  var filteredValues = values.filter(function(x) {
+      return (x <= maxValue) && (x >= minValue);
+  });
 
-    // Then return
-    return filteredValues;
+  return filteredValues;
 }
