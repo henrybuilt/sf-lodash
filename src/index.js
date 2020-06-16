a8281f9ad90d08312f6512fab0da5943d881f3af
Chris <cbugge@henrybuilt.com>
2020-06-16 13:26:07 -0700

    added to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import lodash from 'lodash';
 
 import findMap from './find-map/find-map';
+import filterOutliers from './filter-outliers/filter-outliers';
 import objectFromKeys from './object-from-keys/object-from-keys';
 
 //WARNING for some reason mocha + babel doesn't like spread syntax
-var _ = Object.assign({}, lodash, {findMap, objectFromKeys});
+var _ = Object.assign({}, lodash, {findMap, filterOutliers, objectFromKeys});
 
 export default _;
