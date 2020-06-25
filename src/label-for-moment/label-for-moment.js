70808e042ae3b9e83c64eca8fa929805831211dd
Farhad <farhad@henrybuilt.com>
2020-06-25 14:54:19 -0700

    remove spave between time and am/pm

--- src/label-for-moment/label-for-moment.js
+++ src/label-for-moment/label-for-moment.js
@@ -3,15 +3,15 @@ import moment from 'moment';
 
 export default function labelForMoment(date) {
   var momentInstance = moment(date);
-  var format = 'M/D/YY HH:mm a'; //standard long form - m/d/y 00:00am
+  var format = 'M/D/YY hh:mma'; //standard long form - m/d/y 00:00am
   var timeDifference = _.round(Math.abs(momentInstance.diff(moment()) / 1000), 0);
   var secondsInOneDay = 60 * 60 * 24;
 
   if (timeDifference < secondsInOneDay) {
-    format = 'HH:mm a'; // today - 00:00am
+    format = 'hh:mma'; // today - 00:00am
   }
   else if (moment().year() === momentInstance.year()) {
-    format = 'M/D HH:mm a'; // this year - m/d 00:00am
+    format = 'M/D hh:mma'; // this year - m/d 00:00am
   }
 
   return momentInstance.format(format);
--- src/label-for-moment/label-for-moment.test.js
+++ src/label-for-moment/label-for-moment.test.js
@@ -6,30 +6,30 @@ describe('_.labelForMoment()', () => {
   it('should return in 00:00am format if the moment is in today', () => {
     var momentInstance = moment().hour(10).minute(0);
 
-    expect(labelForMoment(momentInstance)).to.equal('10:00 am');
+    expect(labelForMoment(momentInstance)).to.equal('10:00am');
 
     momentInstance.add(12, 'hours');
 
-    expect(labelForMoment(momentInstance)).to.equal('22:00 pm');
+    expect(labelForMoment(momentInstance)).to.equal('10:00pm');
   });
 
   it('should return in m/d 00:00am format if the moment is in this year but not today', () => {
     var momentInstance = moment(`04/26/${moment().year()}`).hour(9).minute(0);
 
-    expect(labelForMoment(momentInstance)).to.equal('4/26 09:00 am');
+    expect(labelForMoment(momentInstance)).to.equal('4/26 09:00am');
 
     momentInstance.add(12, 'hours');
 
-    expect(labelForMoment(momentInstance)).to.equal('4/26 21:00 pm');
+    expect(labelForMoment(momentInstance)).to.equal('4/26 09:00pm');
 
     momentInstance.add(24, 'hours');
 
-    expect(labelForMoment(momentInstance)).to.equal('4/27 21:00 pm');
+    expect(labelForMoment(momentInstance)).to.equal('4/27 09:00pm');
   });
 
   it('should return in m/d 00:00am format if the moment is in another year', () => {
     var momentInstance = moment('04/26/1995').hour(9).minute(0);
 
-    expect(labelForMoment(momentInstance)).to.equal('4/26/95 09:00 am');
+    expect(labelForMoment(momentInstance)).to.equal('4/26/95 09:00am');
   });
 });
